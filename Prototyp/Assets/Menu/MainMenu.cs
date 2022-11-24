a3335c8df8f09081061d901ac602d46c9fad7d71
sa6659s <julian.addicks@alumni.fh-aachen.de>
2022-11-24 14:57:47 +0100

    Main Menu

---
+++ Prototyp/Assets/Menu/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
