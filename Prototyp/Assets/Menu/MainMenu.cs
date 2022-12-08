9dcc7af154d76bb3fd6eceffd4ba9e63a5a048b8
sa6659s <julian.addicks@alumni.fh-aachen.de>
2022-12-08 13:29:03 +0100

    Level dunkel,licht prefab,menü

--- Prototyp/Assets/Menu/MainMenu.cs
+++ Prototyp/Assets/Menu/MainMenu.cs
@@ -13,6 +13,18 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(2);
     }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene(5);
+    }
     public void QuitGame()
     {
         Application.Quit();
